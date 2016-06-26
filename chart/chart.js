1db44cc6d0499c5b948dba4a4059ed834e73519b
whlinw <whlin.w@gmail.com>
2016-06-26 19:36:09 +0800

    chart: dotted line

--- chart/chart.js
+++ chart/chart.js
@@ -15,33 +15,37 @@ function showChart() {
     data: {
       labels: dates,
       datasets: [{
-        label: stations[s],
         fill: false,
-        lineTension: 0.1,
         backgroundColor: "rgba(0,51,153,0.4)",
         borderColor: "rgba(0,51,153,1)",
-        borderCapStyle: 'butt',
-        borderDash: [],
-        borderDashOffset: 0.0,
-        borderJoinStyle: 'miter',
         pointBorderColor: "rgba(0,51,153,1)",
         pointBackgroundColor: "#fff",
-        pointBorderWidth: 1,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "rgba(0,51,153,1)",
-        pointHoverBorderColor: "rgba(220,220,220,1)",
-        pointHoverBorderWidth: 2,
         pointRadius: 1,
-        pointHitRadius: 10,
-        data: station_datas[s],
-      }]
+        data: station_datas[s].slice(0,486)
+      },
+      {
+        fill: false,
+        backgroundColor: "rgba(0,51,153,0.4)",
+        borderColor: "rgba(0,51,153,1)",
+        borderDash: [1, 1],
+        pointRadius: 1,
+        data: station_datas[s]
+      }
+      ]
     },
     options: {
       responsive: false,
+      title: {
+        display: true,
+        text: stations[s]
+      },
       scales: {
         xAxes: [{
-          display: false
+          display: true
         }]
+      },
+      legend: {
+        display: false
       }
     }
   });
